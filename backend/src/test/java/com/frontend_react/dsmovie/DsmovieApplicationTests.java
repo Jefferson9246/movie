365e813fe4b7da3320d5a07f3d23e7aaa565e72b
Jefferson9246 <jeffersonrodrigomoreira@gmail.com>
2022-03-12 19:20:53 -0400

    Project created

---
+++ backend/src/test/java/com/frontend_react/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frontend_react.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
