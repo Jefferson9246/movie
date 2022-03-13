53ea8a2d0412164a36543e432107fc2b0f3e60b0
Jefferson <jeffersonrodrigomoreira@gmail.com>
2022-03-13 08:43:15 -0400

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,24 @@
-import NavBar from "./components/NavBar";
+import { //importando rotas com rect-router-dom para setar qual evento segue qual rota
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "components/NavBar";
 
 function App() {
   return (
-    <NavBar />
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} /> {/* setando como o path '/' sendo nossa página inicial */}
+        <Route path="/form"> {/* setando que quando tiver o /form vai ser direcionado para o movieId e levar pra pagina form criada */}
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
