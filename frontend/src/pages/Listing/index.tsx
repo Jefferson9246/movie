40f9231afb1fedecb862c8868098cccb0f23f9e3
Jefferson <jeffersonrodrigomoreira@gmail.com>
2022-03-13 20:09:17 -0400

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -3,15 +3,22 @@ import MovieCard from "components/MovieCard";
 import MovieScore from "components/MovieScore";
 import MovieStar from "components/MovieStars";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    axios.get(`${BASE_URL}/movies?size=12&page=0`).then(
-        response => {
-            console.log(response.data);
-        }
-    )
+    const [pageNumber, setPageNumber] = useState(0); /* mantem o estado do componente */
+    useEffect(() => { //vai executar a function somente quando o componente estiver terminado 
+        axios.get(`${BASE_URL}/movies?size=12&page=0`).then(
+            response => {
+                const data = response.data as MoviePage;
+                setPageNumber(data.number);
+                console.log(response.data);
+            }
+        )
+    })
     return (
         <>
             <Pagination />
